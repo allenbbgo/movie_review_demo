f3b4ef6bfd59a4d760b286696eb3c4d54fff2709
allenbbgo <c8c886@gmail.com>
2019-02-18 16:17:27 +0800

    add paperclip for image upload

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
     belongs_to :users
-
+    has_attached_file :image, :styles => { medium: "400x600#"  }
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
